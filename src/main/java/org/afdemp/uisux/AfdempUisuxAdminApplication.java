de40698e5865b171d54c94526b5b985b62cc8975
padoura <padoura@users.noreply.github.com>
2018-01-21 16:23:03 +0200

    Initialize Spring Boot Project

---
+++ src/main/java/org/afdemp/uisux/AfdempUisuxAdminApplication.java
@@ -0,0 +1,12 @@
+package org.afdemp.uisux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfdempUisuxAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfdempUisuxAdminApplication.class, args);
+	}
+}
