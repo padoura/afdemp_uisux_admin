f6581d025f39886167faedfa09985e6f2ae6dbcc
lefteris08 <33422312+lefteris08@users.noreply.github.com>
2018-01-28 16:13:12 +0200

    created remove single in product some minor problems and add user

--- src/main/resources/static/js/beetwensearch.js
+++ src/main/resources/static/js/beetwensearch.js
@@ -30,6 +30,34 @@ $(document).ready(function() {
 		});
 	});
 	
+
+	$('.delete-product').on('click', function (){
+		/*<![CDATA[*/
+	    var path = /*[[@{/}]]*/'remove';
+	    /*]]>*/
+		
+		var id=$(this).attr('id');
+		
+		bootbox.confirm({
+			message: "Are you sure to remove this ? It can't be undone.",
+			buttons: {
+				cancel: {
+					label:'<i class="fa fa-times"></i> Cancel'
+				},
+				confirm: {
+					label:'<i class="fa fa-check"></i> Confirm'
+				}
+			},
+			callback: function(confirmed) {
+				if(confirmed) {
+					$.post(path, {'id':id}, function(res) {
+						location.reload();
+					});
+				}
+			}
+		});
+	});
+	
 	
 	
 //	$('.checkboxBook').click(function () {
