2eb224dab92c8d12fe84222f1781663d94c97aea
Madryoch <madryoch@hotmail.com>
2018-01-31 21:42:10 +0200

    non Functional List<ClientOrder> findOrdersOfProductXFromTo(@Param("from") Timestamp from, @Param("to") Timestamp to,@Param("item") Product product);
    in ClientOrderrepository.

--- src/main/java/org/afdemp/uisux/repository/ClientOrderRepository.java
+++ src/main/java/org/afdemp/uisux/repository/ClientOrderRepository.java
@@ -3,9 +3,8 @@ package org.afdemp.uisux.repository;
 import java.sql.Timestamp;
 import java.util.List;
 
-import javax.persistence.TemporalType;
-
 import org.afdemp.uisux.domain.ClientOrder;
+import org.afdemp.uisux.domain.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +12,10 @@ import org.springframework.data.repository.query.Param;
 public interface ClientOrderRepository extends CrudRepository<ClientOrder, Long> {
 
 	@Query("SELECT c FROM ClientOrder c WHERE c.submittedDate BETWEEN :from AND :to")
-    public List<ClientOrder> findOrdersFromTo(@Param("from") Timestamp from, @Param("to") Timestamp to);
+    List<ClientOrder> findOrdersFromTo(@Param("from") Timestamp from, @Param("to") Timestamp to);
+	
+	//@Query("SELECT a from SELECT co FROM ClientOrder co INNER JOIN CartItem ci ON co=ci.abstractSale WHERE co.submittedDate BETWEEN :from AND :to AND ci.product=item") 
+	//List<ClientOrder> findOrdersOfProductXFromTo(@Param("from") Timestamp from, @Param("to") Timestamp to,@Param("item") Product product);
 	
 	
 }
