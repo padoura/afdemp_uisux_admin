90e3c01fa78bf1cca82c7890b8eaa3509b851913
padoura <padoura@users.noreply.github.com>
2018-02-05 01:28:00 +0200

    changed stockUp2 to stockUp

--- src/main/java/org/afdemp/uisux/controller/ProductController.java
+++ src/main/java/org/afdemp/uisux/controller/ProductController.java
@@ -166,13 +166,13 @@ public class ProductController {
 		return "stockUpConfirm";
 	}
 	
-	@RequestMapping(value = "/stockUp2")
+	@RequestMapping(value = "/stockUp", method = RequestMethod.POST)
 	public String stockUpPost(@ModelAttribute("memberCartItemId") Long memberCartItemId,
-			@ModelAttribute("qty") int qty,
+			@ModelAttribute("qty") Integer qty,
 			Model model) {
 		
 		MemberCartItem memberCartItem = memberCartItemService.findById(memberCartItemId);
-	//	Integer qty= Integer.valueOf(qty2);
+		
 		if (qty > memberCartItem.getQty()) {
 			model.addAttribute("requestExceedsAvailability", true);
 			return "productList";
@@ -183,7 +183,7 @@ public class ProductController {
 		}
 		
 		model.addAttribute("stockUpSuccess", true);
-		return "productList";
+		return "redirect:/product/productList";
 	}
 
 
