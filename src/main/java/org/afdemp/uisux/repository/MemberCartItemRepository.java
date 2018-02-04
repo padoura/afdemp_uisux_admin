42d509871c7dafcdd71ce0bda48cef491dc0c356
padoura <padoura@users.noreply.github.com>
2018-02-05 00:51:57 +0200

    Fixed issues with purchase queries and set visibility to false in new sold item for partialPurchase
    TODO:
    
    Change visibility to false for fullPurchase too

--- src/main/java/org/afdemp/uisux/repository/MemberCartItemRepository.java
+++ src/main/java/org/afdemp/uisux/repository/MemberCartItemRepository.java
@@ -24,11 +24,13 @@ MemberCartItem findByShoppingCartAndProduct(ShoppingCart shoppingCart, Product p
 	
 	
 	@Modifying
+	@Transactional
 	@Query("UPDATE MemberCartItem mci SET mci.qty=mci.qty-:qty WHERE mci.product=:product AND mci.qty>:qty AND mci.shoppingCart=:shoppingCart")
-	int partialPurchase(@Param("product") Product product,@Param("qty") long qty, @Param("shoppingCart") ShoppingCart shoppingCart);
+	int partialPurchase(@Param("product") Product product,@Param("qty") int qty, @Param("shoppingCart") ShoppingCart shoppingCart);
 	
 	
 	@Modifying
+	@Transactional
 	@Query("UPDATE MemberCartItem mci SET mci.abstractSale=:sale,mci.shoppingCart=null WHERE mci.product=:product AND mci.shoppingCart=:shoppingCart")
 	int fullPurchase(@Param("product") Product product, @Param("shoppingCart") ShoppingCart shoppingCart,@Param("sale") AbstractSale abstractSale);
 	
